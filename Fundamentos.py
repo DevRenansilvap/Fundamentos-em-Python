bcc6264434f4cee42d147fd99e8f560a19b4663b
DevRenans <renansilvapa@gmail.com>
2026-08-15 12:34:10 -0300

    Adicionado mais linha de codigos/exercicios em python

--- Fundamentos.py
+++ Fundamentos.py
@@ -8,8 +8,8 @@ altura = 1.80
 
 print(f"Meu nome é {nome} eu tenho {idade} e tenho {altura} de altura")
 
-# Peça ao usuário para digitar dois números (use input()).
-# Converta ambos para int e imprima a soma, subtração, multiplicação e divisão.
+# # Peça ao usuário para digitar dois números (use input()).
+# # Converta ambos para int e imprima a soma, subtração, multiplicação e divisão.
 
 num1 = input("Digite o Primeiro numero:")
 num2 = input("Digite o Segundo numero:")
@@ -21,7 +21,7 @@ divisao = int(num1)/int(num2)
 
 print(f"A soma é:{soma} a subtração é:{subtracao} a multiplicacao é:{multiplicacao} a divisao é:{divisao}")
 
-# Usando a função type() para descobrir o tipo de variavel
+# # Usando a função type() para descobrir o tipo de variavel
 
 a = 10
 b = 3.14
@@ -35,9 +35,29 @@ print(f"O tipo da variável c é: {type(c)}")
 print(f"O tipo da variável d é: {type(d)}")
 print(f"O tipo da variável e é: {type(e)}")
 
-# Usando slicing
+# # Usando slicing
 nome = "Renan da Silva"
 print(f"As três primeiras letras são:{nome[:3]}")
 print(f"As três primeiras letras são:{nome[11:14]}")
 print(f"As três primeiras letras são:{nome[::-1]}")
+
+# # Metodos de Manipulação de Strings
+nome = ' Renan da Silva '
+print(nome.strip()) # Remove espaços em branco no início e no final da string
+print(nome.upper()) # Converte a string para maiúsculas
+print(nome.lower()) # Converte a string para minúsculas
+print(nome.count("e")) # Conta quantas vezes a letra "e" aparece na string
+
+
+#concatenação e interpolação de strings
+nome = "Renan"
+idade = 23
+print("meu nome é " + nome + "eu tenho " + str(idade) + " anos")
+print(f"meu nome é {nome} eu tenho {idade} anos")
+
+frase = input("Digite uma frase:")
+print(len(frase))
+print('Tem Python' in frase else 'Não tem Python')
+print(frase.replace('renanzinho ', 'Renan da Silva',))
+
 
